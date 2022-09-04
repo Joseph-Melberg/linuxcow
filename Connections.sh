e9c2654f57ea5f8518a9492ebd7f875be506353c
Joseph-Melberg <dasbasdo@gmail.com>
2022-09-04 18:38:00 -0500

    Even more updates

---
+++ Connections.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ssh-copy-id -o "StrictHostKeyChecking=no" tool
+ssh-copy-id -o "StrictHostKeyChecking=no" centurionx
+ssh-copy-id -o "StrictHostKeyChecking=no" jump
+ssh-copy-id -o "StrictHostKeyChecking=no" speed
